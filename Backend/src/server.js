b9f79385c0dd1823bde6f1dbb80fa69c2d33e0f8
agrawalpankaj1312-sketch <agrawalharshit5965@gmail.com>
2026-09-02 10:32:17 +0530

    Some Features Added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -17,13 +17,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    process.env.FRONTEND_URL,
-  ],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
